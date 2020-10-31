73ddff758a3d3fb7db4d828e5fb12f5da1f0e792
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-10-30 21:40:58 -0400

    commit tree 72111aa626ca9f2ccdae3531b3b77c36faa690c0

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+    if arr.length == 1
+        arr.join
+    elsif arr.length == 2
+        arr.join(" and ")
+    else
+        # put a comma after each, but add "and" before the last word
+        arr.each.collect do |value|
+            if value == arr[-1]
+                arr[-1] = "and " << value
+            end
+        end
+        arr.join(", ")
+    end
 end
